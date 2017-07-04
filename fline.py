218158ff55b6836ad632336956d09ef26334d868
stewart <fongzeo@gmail.com>
2017-07-04 17:09:08 +0800

    plot

--- fline.py
+++ fline.py
@@ -1,4 +1,5 @@
 import re
+import pylab as pl
 bds = input('please enter the name of the data file: ')
 di = input('please input the value of d: ')
 di = float(di)
@@ -11,14 +12,48 @@ with open(bds,'r') as f:
 #print(z)
 for i in range(2):
     z[i] = re.split(r'[\s\,]+',z[i].strip())
-    print(z[i])
+    #print(z[i])
     if z[i][-1] == '':
        z[i].pop()
-    print(z[i])
+    #print(z[i])
     for j in range(len(z[i])):
         z[i][j] = float(z[i][j])
-        print(z[i][j])
-            
+        #print(z[i][j])
+x = z[0]
+y = z[1]
+xl = x[-1]-x[0]
+pl.plot(x,y)
+pl.show() 
+n = len(x)
+nn = n-1
+a = []
+e = []
+c = []
+f = []
+xx = []
+yy = []
+for k in range(kk):
+    for i in range(nn):
+        a.append((x[i+1]-x[i])/xl)
+        e.append((x[-1]*x[i]-x[0]*x[i+1])/xl)
+        c.append((y[i+1]-y[i]-di*(y[-1]-y[0]))/xl)
+        f.append(((x[-1]*y[i]-x[0]*y[i+1])-di*(x[-1]*y[0]-x[0]*y[-1]))/xl)
+    for i in range(nn):
+        for j in range(n):
+            xx.append(a[i]*x[j]+e[i])
+            yy.append(c[i]*x[j]+di*y[j]+f[i])
+    n = len(xx)
+    nn = n-1
+    x = xx
+    y = yy
+    xx = []
+    yy = []
+    a = []
+    e = []
+    c = []
+    f = []
+pl.plot(x,y)
+pl.show()
             
             
         
