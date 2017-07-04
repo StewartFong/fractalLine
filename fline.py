da6a76040af5fe593d17108347994ec3dc1b1647
stewart <fongzeo@gmail.com>
2017-07-04 13:16:11 +0800

    input and output

--- fline.py
+++ fline.py
@@ -1,5 +1,25 @@
-bds = input('please enter the name of the data file')
-di = input('please input the value of d')
-di = int(di)
-kk = input('Please input the iterational times')
+import re
+bds = input('please enter the name of the data file: ')
+di = input('please input the value of d: ')
+di = float(di)
+#print(di)
+kk = input('Please input the iterational times: ')
 kk = int(kk)
+with open(bds,'r') as f:
+     z = f.readlines()
+     #print(z)
+#print(z)
+for i in range(2):
+    z[i] = re.split(r'[\s\,]+',z[i].strip())
+    print(z[i])
+    if z[i][-1] == '':
+       z[i].pop()
+    print(z[i])
+    for j in range(len(z[i])):
+        z[i][j] = float(z[i][j])
+        print(z[i][j])
+            
+            
+            
+        
+        
